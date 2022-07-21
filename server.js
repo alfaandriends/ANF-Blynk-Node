2754cd18dab3834ee3fe4c51320457fcfaf2c5a7
JARRAKAL\Jarrakal <abdulroaf628@gmail.com>
2022-07-21 14:41:49 +0800

    added index.js

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var app = express()
+
+/* Blynk Config */
+var blynk_2 = require("blynk");
+var blynk_1 = new blynk_2['default']('54.251.129.244', 9443);
+blynk_1.connect();
+
+/* Route (url) */
+app.get('/login', function(req, res){
+    
+    let login =  blynk_1.login('admin@blynk.cc', 'admin');
+    login.then(function(result){
+        res.send(result);
+    })
+})
+
+app.get('/register', function(req, res){
+
+    let login =  blynk_1.login('awan@alfaandfriends.com', '123123');
+    login.then(function(result){
+        res.send(result);
+    })
+})
+
+app.get('/profile', function(req, res){
+    let profile =  blynk_1.loadProfileGzipped();
+    profile.then(function(result){
+        res.send(result);
+    })
+})
+
+/* Listen Port */
+app.listen(3000, () => console.log('Listening to port 3000'))
