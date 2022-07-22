467d383034adfd4d2be25a469c0ae847275b01c0
JARRAKAL\Jarrakal <abdulroaf628@gmail.com>
2022-07-22 17:17:56 +0800

    added new commmand for register

--- server.js
+++ server.js
@@ -4,27 +4,30 @@ var app = express()
 /* Blynk Config */
 var blynk_2 = require("blynk");
 var blynk_1 = new blynk_2['default']('54.251.129.244', 9443);
-blynk_1.connect();
 
 /* Route (url) */
 app.get('/login/:email/:password', function(req, res){
+    blynk_1.connect();
     let login =  blynk_1.login(req.params.email, req.params.password);
-    let profile =  blynk_1.loadProfileGzipped();
-    profile.then(function(result){
-        res.send(result);
+    login.then(function(result){
+        res.send(result+"");
     })
 })
 
-app.get('/register', function(req, res){
+app.get('/register/:email/:password', function(req, res){
 
-    let login =  blynk_1.login('awan@alfaandfriends.com', '123123');
-    login.then(function(result){
-        res.send(result);
+    let register =  blynk_1.login(req.params.email, req.params.password);
+    register.then(function(result){
+        res.send(result+"");
     })
 })
 
 app.get('/profile', function(req, res){
+    let profile =  blynk_1.loadProfileGzipped();
+    profile.then(function(result){
+        res.send(result+"");
+    })
 })
 
 /* Listen Port */
-app.listen(8000, () => console.log('Listening to port 8000'))
+app.listen(8000, () => console.log('Listening to port 8000 at http://127.0.0.1:8000'))
