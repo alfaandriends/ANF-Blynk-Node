6d3d3ba987bfd5fe7c1a7b08cb9fed78a2c46d7e
Abdul Raof-PC <abdulraof628@gmail.com>
2022-07-24 03:12:09 +0800

    added new command

--- server.js
+++ server.js
@@ -6,27 +6,31 @@ var blynk_2 = require("blynk");
 var blynk_1 = new blynk_2['default']('54.251.129.244', 9443);
 
 /* Route (url) */
-app.get('/login/:email/:password', function(req, res){
+app.get('/login/:email/:password', function(req, res) {
     blynk_1.connect();
-    let login =  blynk_1.login(req.params.email, req.params.password);
-    login.then(function(result){
-        res.send(result+"");
+    let login = blynk_1.login(req.params.email, req.params.password);
+    login.then(function(result) {
+        res.send(result + "");
     })
 })
 
-app.get('/register/:email/:password', function(req, res){
-
-    let register =  blynk_1.login(req.params.email, req.params.password);
-    register.then(function(result){
-        res.send(result+"");
+app.get('/register/:email/:password', function(req, res) {
+    blynk_1.connect();
+    let register = blynk_1.register(req.params.email, req.params.password);
+    register.then(function(result) {
+        res.send(result + "");
     })
 })
 
-app.get('/profile', function(req, res){
-    let profile =  blynk_1.loadProfileGzipped();
-    profile.then(function(result){
-        res.send(result+"");
+app.get('/profile', function(req, res) {
+    let profile = blynk_1.loadProfileGzipped();
+    profile.then(function(result) {
+        res.send(result);
     })
+})
+
+app.get('/logout', function(req, res){
+    blynk_1.disconnect();
 })
 
 /* Listen Port */
